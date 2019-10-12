e4bc4633f62a5e8bd8316f3974a263e9ab031857
Wen <34200399+FeronCompany@users.noreply.github.com>
2019-10-12 19:28:31 +0800

    update BaseObject.h
    add runtime method register

--- ObjectManager/include/BaseObject.h
+++ ObjectManager/include/BaseObject.h
@@ -43,3 +43,58 @@ T* SafeNew()
 	}
 	return ptr;
 }
+
+map<string, void*> aaMap;
+
+class RegObject
+{
+public:
+	RegObject(const string& key, void* func)
+	{
+		aaMap[key] = func;
+	}
+};
+
+#define DECLARE_MEMBER_FUNC(class_name, func_name) \
+	void func_name(int num); \
+	static void st_##func_name(class_name* obj, int num) \
+	{ \
+		obj->func_name(num);\
+	}
+
+#define REG_MEMBER_FUNC(class_name, func_name) \
+	RegObject regObj##class_name##func_name(#func_name, (void*)class_name::st_##func_name);
+
+class TestRegFunc
+{
+public:
+	TestRegFunc() :health(0) {}
+	DECLARE_MEMBER_FUNC(TestRegFunc, healthMod);
+
+	void tell()
+	{
+		cout << "health[" << health << "]\n";
+	}
+
+private:
+	int32_t health;
+};
+
+void TestRegFunc::healthMod(int num)
+{
+	this->health += num;
+}
+
+REG_MEMBER_FUNC(TestRegFunc, healthMod);
+
+typedef void (*member_func)(TestRegFunc*, int);
+
+void testRegFunc()
+{
+	TestRegFunc aaa;
+	aaa.tell();
+	void* func = aaMap["healthMod"];
+	static_cast<member_func>(func)(&aaa, 5);
+	aaa.tell();
+}
+
